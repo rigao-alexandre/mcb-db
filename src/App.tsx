de3236496e0c5e663d12db8f1800dd20786f8800
Alexandre <rigao.alexandre@gmail.com>
2022-03-22 00:47:49 -0300

    adicionar light mode | dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 /** @format */
 
-import React from 'react';
+import * as React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import Box from '@mui/material/Box';
+import {useTheme, ThemeProvider, createTheme} from '@mui/material/styles';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+
+const ColorModeContext = React.createContext({toggleColorMode: () => {}});
 
 function App() {
+   const theme = useTheme();
+   const colorMode = React.useContext(ColorModeContext);
+
    return (
       <div className='App'>
          <header className='App-header'>
@@ -18,10 +27,55 @@ function App() {
                Learn React
             </a>
             <br />
-            <Button variant='contained'>Olá Mundo</Button>
+            <Box
+               sx={{
+                  display: 'flex',
+                  width: '100%',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  bgcolor: 'background.default',
+                  color: 'text.primary',
+                  borderRadius: 1,
+                  p: 3,
+               }}>
+               {theme.palette.mode} mode
+               <IconButton sx={{ml: 1}} onClick={colorMode.toggleColorMode} color='inherit'>
+                  {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
+               </IconButton>
+            </Box>
          </header>
       </div>
    );
 }
 
-export default App;
+function AppContainer() {
+   const [mode, setMode] = React.useState<'light' | 'dark'>('light');
+   const colorMode = React.useMemo(
+      () => ({
+         toggleColorMode: () => {
+            setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+         },
+      }),
+      []
+   );
+
+   const theme = React.useMemo(
+      () =>
+         createTheme({
+            palette: {
+               mode,
+            },
+         }),
+      [mode]
+   );
+
+   return (
+      <ColorModeContext.Provider value={colorMode}>
+         <ThemeProvider theme={theme}>
+            <App />
+         </ThemeProvider>
+      </ColorModeContext.Provider>
+   );
+}
+
+export default AppContainer;
