2301e78200b29d99569e2e12f2bfe3bb25051f5b
Alexandre <rigao.alexandre@gmail.com>
2022-03-22 01:05:12 -0300

    adicionar AppBar

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,24 @@ import * as React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import IconButton from '@mui/material/IconButton';
 import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+
 import {useTheme, ThemeProvider, createTheme} from '@mui/material/styles';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
 
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import MenuIcon from '@mui/icons-material/Menu';
+
 const ColorModeContext = React.createContext({toggleColorMode: () => {}});
 
 function App() {
-   const theme = useTheme();
-   const colorMode = React.useContext(ColorModeContext);
-
    return (
       <div className='App'>
+         <DenseAppBar />
          <header className='App-header'>
             <img src={logo} className='App-logo' alt='logo' />
             <p>
@@ -26,25 +30,32 @@ function App() {
             <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
                Learn React
             </a>
-            <br />
-            <Box
-               sx={{
-                  display: 'flex',
-                  width: '100%',
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                  bgcolor: 'background.default',
-                  color: 'text.primary',
-                  borderRadius: 1,
-                  p: 3,
-               }}>
-               {theme.palette.mode} mode
+         </header>
+      </div>
+   );
+}
+
+function DenseAppBar() {
+   const theme = useTheme();
+   const colorMode = React.useContext(ColorModeContext);
+
+   return (
+      <Box sx={{flexGrow: 1}}>
+         <AppBar position='static'>
+            <Toolbar variant='dense'>
+               <IconButton edge='start' color='inherit' aria-label='menu' sx={{mr: 2}}>
+                  <MenuIcon />
+               </IconButton>
+               <Typography variant='h6' color='inherit' component='div'>
+                  MasterChef Brasil
+               </Typography>
+               <Box sx={{flexGrow: 1}} />
                <IconButton sx={{ml: 1}} onClick={colorMode.toggleColorMode} color='inherit'>
                   {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
                </IconButton>
-            </Box>
-         </header>
-      </div>
+            </Toolbar>
+         </AppBar>
+      </Box>
    );
 }
 
